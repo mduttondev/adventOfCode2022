cf3b59534977e0dcdc49a4992271f99be4e72d95
Matthew Dutton <mduttondev@gmail.com>
2022-12-01 14:31:06 -0500

    day 1

--- day1/day_1.rb
+++ day1/day_1.rb
@@ -1,22 +1,16 @@
-def read_input
-  highest_calories = 0
+sums = []
 
-  sums = []
-
-  accumulator = 0
-  File.readlines('./day_1_input.txt').each do |line|
-    if line.to_s.strip.empty?
-      sums.push(accumulator)
-      accumulator = 0
-    else
-      accumulator += line.to_i
-    end
+accumulator = 0
+File.readlines('./day_1_input.txt').each do |line|
+  if line.to_s.strip.empty?
+    sums.push(accumulator)
+    accumulator = 0
+  else
+    accumulator += line.to_i
   end
-
-  sums.sort!.reverse!
-
-  puts 'Top 3 highest_calories'
-  puts [sums[0], sums[1], sums[2]].sum
 end
 
-read_input
+sums.sort!.reverse!
+
+puts 'Top 3 highest_calories'
+puts [sums[0], sums[1], sums[2]].sum
