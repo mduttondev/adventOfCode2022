8120ec3a888fb123741cb8a9a308c1cf0fbf83f5
Matthew Dutton <mduttondev@gmail.com>
2022-12-11 15:24:03 -0500

    day_10_rb part 2 done

--- day_10_rb/day_10.rb
+++ day_10_rb/day_10.rb
@@ -5,10 +5,17 @@ def cycle_check(cycle_num, reg, strength)
   return strength unless cycle_num == 20 || ((cycle_num + 20) % 40).zero?
 
   cycle_strength = cycle_num.to_i * reg.to_i
-  new_strength = cycle_strength + strength
+  new_strength = cycle_strength.to_i + strength.to_i
   puts "cycle: #{cycle_num} || Strength: #{cycle_strength} || Strength Sum: #{new_strength}"
 
   new_strength
+end
+
+def check_draw_pixel(cycle_num, reg)
+  pixel = cycle_num % 40
+  return false unless pixel.between?((reg - 1), (reg + 1))
+
+  true
 end
 
 def read_file(path)
@@ -26,21 +33,32 @@ file_path = '/input.txt'
 # file_path = '/demo.txt'
 
 register = 1
-cycle = 1
+cycle = 0
 
 strength_sum = 0
 
 lines = read_file(file_path)
 
+drawing = String.new
+
 lines.each do |line|
   cycle += 1
+
   strength_sum = cycle_check(cycle, register, strength_sum)
+
+  draw = check_draw_pixel(cycle, register)
+  drawing += draw ? '#' : ' '
 
   next unless line.start_with?('addx')
 
   cycle += 1
-  parts = line.split(' ')
-  register += parts[1].to_i
 
   strength_sum = cycle_check(cycle, register, strength_sum)
+
+  register += line.split(' ')[1].to_i
+
+  draw = check_draw_pixel(cycle, register)
+  drawing += draw ? '#' : ' '
 end
+
+puts drawing.chars.each_slice(40).map(&:join)
